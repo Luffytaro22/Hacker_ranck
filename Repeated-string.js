10f43be86a8da8848ffae2e8de52a1eb52129feb
Luffytaro22 <manuelsanchez059@gmail.com>
2023-02-25 12:35:33 -0500

    Un poco mejor, pero no suficiente

--- Repeated-string.js
+++ Repeated-string.js
@@ -2,11 +2,15 @@
 function repeatedString(s, n) {
     // Write your code here
     var count_a = 0;
+    var s1;
     if(s === 'a'){
         return n;
     }else{
-    var s1 = s.repeat(n/2);
-    console.log(s1[1])
+        if(n > s.length){
+            s1 = s.repeat(Math.ceil(n/s.length));
+        }else{
+            s1 = s.repeat(n/2);
+        }
     for(let i = 0; i < n; i++){
         if(s1[i] == 'a'){
             count_a++;
