7d446669161fd96924393264d83a716dfe2624eb
Luffytaro22 <manuelsanchez059@gmail.com>
2023-03-03 12:53:34 -0500

    Agregada funcion sort y da mejores resultados. Pero no cumple con lo que se pide.

--- New-Year-Chaos.js
+++ New-Year-Chaos.js
@@ -1,9 +1,28 @@
 function minimumBribes(q) {
     // Write your code here
     let count = 0;
+    let result;
+    //Con esta función se crea un nuevo arrelgo "sort" sin alterar el arreglo original "q".
+    function sorted(q){
+        let sort = [...q];
+        return sort.sort(function(a,b){return a - b});
+    }
+    console.log(q)
+    console.log(sorted(q))
     if(q[0] == q.length){
         console.log('Too chaotic');
-    }else{
+    }
+    for(let i = 0; i < q.length; i++){
+        if(q[i] != sorted(q)[i]){
+            if(Math.abs(q.indexOf(sorted(q)[i]) - i) >= 3){
+                result = 'Too chaotic';
+                break;
+            }else{result = ''}
+        }
+    }
+    if(result == 'Too chaotic'){console.log('Too chaotic')}    
+    else{
+        
         for(let j = 0; j < Math.floor(q.length/2);j++){
             for(let i = 0; i < q.length - 1; i++){
                 if(q[i + 1] < q[i]){
@@ -14,11 +33,7 @@ function minimumBribes(q) {
                 }
             }
         }
-        if(q[q.length-1] - q[0] != q.length - 1){
-            console.log('Too chaotic');
-        }else{
-            console.log(count);
-        }
+        console.log(count);
     }
 }
 minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])
