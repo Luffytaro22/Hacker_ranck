3f408b23bde2debb314ff1b2ba5aa26c8ccd0f4e
Luffytaro22 <manuelsanchez059@gmail.com>
2023-03-09 12:45:55 -0500

    Terminado implementando las soluciones del cuadrado magico

--- Forming a Magic Square.js
+++ Forming a Magic Square.js
@@ -1,83 +1,27 @@
 function formingMagicSquare(s) {
     // Write your code here
-    let diferencia_filas;
-    let diferencia_columnas;
-    let diferencia_diagonal;
-    let result_columnas = [];
-    let result_filas = [];
-    let result_diagonal = [];
-    let count = 0;
-    do{
-    count = 0;
-    for(let i = 0; i < s.length; i++){
-        result_filas[i] = [];
-        if(s[i][0] + s[i][1] + s[i][2] != 15){
-            count += 1;
-            diferencia_filas = Math.abs(s[i][0] + s[i][1] + s[i][2] - 15);
-            result_filas[i].push(diferencia_filas);
-            if(s[i][0] + diferencia_filas <= 9){
-                s[i][0] = s[i][0] + diferencia_filas;
-            }else{
-                if(s[i][1] + diferencia_filas <= 9){
-                    s[i][1] = s[i][1] + diferencia_filas;
-                }else{
-                    s[i][2] = s[i][2] + diferencia_filas;
-                }
-                }
-        }
-    }
-    for(let i = 0; i < s.length; i++){
-        result_columnas[i] = [];
-        if(s[0][i] + s[1][i] + s[2][i] != 15){
-            count += 1;
-            diferencia_columnas = Math.abs(s[0][i] + s[1][i] + s[2][i] - 15);
-            result_columnas[i].push(diferencia_columnas);
-            if(s[0][i] + diferencia_columnas <= 9){
-                s[0][i] = s[0][i] + diferencia_columnas;
-            }else{
-                if(s[1][i] + diferencia_columnas <= 9){
-                    s[1][i] = s[1][i] + diferencia_columnas;
-                }else{
-                    s[2][i] = s[2][i] + diferencia_columnas;
+    let square1 = [[4,3,8],[9,5,1],[2,7,6]];
+    let square1_mirror = [[8,3,4],[1,5,9],[6,7,2]];
+    let square2 = [[2,9,4],[7,5,3],[6,1,8]];
+    let square2_mirror = [[4,9,2],[3,5,7],[8,1,6]];
+    let square3 = [[6,7,2],[1,5,9],[8,3,4]];
+    let square3_mirror = [[2,7,6],[9,5,1],[4,3,8]];
+    let square4 = [[8,1,6],[3,5,7],[4,9,2]];
+    let square4_mirror = [[6,1,8],[7,5,3],[2,9,4]];
+    let magic_squares = [square1, square1_mirror, square2, square2_mirror, square3, square3_mirror, square4, square4_mirror];
+    console.log(s[0].indexOf(s[0][1]))
+    let dif = new Array(8).fill(0);
+    
+    for(let i = 0; i < magic_squares.length; i++){
+        for(let j = 0; j < 3; j++){
+            for(let k = 0; k < 3; k++){
+                if(magic_squares[i][j][k] != s[j][k]){
+                    dif[i] += Math.abs(magic_squares[i][j][k] - s[j][k])
                 }
             }
         }
     }
-   
-        if(s[0][0] + s[1][1] + s[2][2] != 15){
-            count += 1;
-            diferencia_diagonal = Math.abs(s[0][0] + s[1][1] + s[2][2] - 15);
-            result_diagonal.push(diferencia_diagonal);
-            if(s[0][0] + diferencia_diagonal <= 9){
-                s[0][0] = s[0][0] + diferencia_diagonal;
-            }else{
-                if(s[1][1] + diferencia_diagonal <= 9){
-                    s[1][1] = s[1][1] + diferencia_diagonal;
-                }else{
-                    s[2][2] = s[2][2] + diferencia_diagonal;
-                }
-            }
-        }
-        if(s[2][0] + s[1][1] + s[0][2] != 15){
-            count += 1;
-            diferencia_diagonal = Math.abs(s[2][0] + s[1][1] + s[0][2] - 15);
-            result_diagonal.push(diferencia_diagonal);
-            if(s[2][0] + diferencia_diagonal <= 9){
-                s[2][0] = s[2][0] + diferencia_diagonal;
-            }else{
-                if(s[1][1] + diferencia_diagonal <= 9){
-                    s[1][1] = s[1][1] + diferencia_diagonal;
-                }else{
-                    s[2][0] = s[2][0] + diferencia_diagonal;
-                }
-            }
-        }
-    }while(count > 0)
-    console.log(s);
-    console.log(result_filas)
-    console.log(result_columnas)
-    result_columnas = result_columnas.map(x => x.reduce((a,b) => a+b,0));
-    result_filas = result_filas.map(x => x.reduce((a,b) => a+b,0));
-    return Math.min(result_columnas, result_filas);
-}
+    console.log(dif)
+    return Math.min(...dif);
+    }
 console.log(formingMagicSquare([[4, 9, 2],[3, 5, 7],[8, 1, 5]]));
